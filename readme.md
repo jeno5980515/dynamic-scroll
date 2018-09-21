fff893d04ad098d47b827cd6d423f068a6b38f79
jeno5980515 <jeno5980515@yahoo.com.tw>
2018-09-21 15:36:15 +0800

    init

---
+++ readme.md
@@ -0,0 +1,11 @@
+# dynamic-scroll
+Render elements dynamically to reduce the amount of DOM elements on the screen.
+
+# ToDo
+1. get scrollbar width
+2. add item dynamically
+3. improve performance
+4. fix edge styles problem
+5. fix blink problem
+6. calculate minimum render amount
+7. user friendly interface
---
+++ src/index.js
@@ -0,0 +1,81 @@
+const createScroll = ({ container, elements }) => {
+  const containerWrapper = document.createElement('div');
+  const wrapper = document.createElement('div');
+  const fakeDiv = document.createElement('div');
+  const containerWidth = container.clientWidth;
+  const containerHeight = container.clientHeight;
+  const borderTop = container.offsetTop;
+  let renderElements = elements;
+  let cache = [];
+
+  // ToDo: calculate minimum render amount
+  let renderCount = 10;
+  let currentIndex = 0;
+
+  // todo: get scrollbar
+  const scrollBarWidth = 17;
+
+  container.style.overflowY = 'scroll';
+
+  containerWrapper.style.position = 'absolute';
+  containerWrapper.style.width = `${containerWidth - scrollBarWidth}px`;
+  containerWrapper.style.height = `${container.clientHeight}px`;
+  containerWrapper.style.overflowY = 'hidden';
+  containerWrapper.style.zIndex = -1;
+
+  wrapper.style.position = 'absolute';
+
+  fakeDiv.style.zIndex = 1;
+
+  containerWrapper.appendChild(wrapper);
+  container.appendChild(containerWrapper);
+  container.appendChild(fakeDiv);
+  
+  const preRender = () => {
+    const content = Array.from(renderElements)
+      .reduce((cal, e) => {
+        // todo: assign attribute
+        return `${cal}<div class="element" style="background: ${e.style.background}">${e.innerHTML}</div>`;
+      }, '');
+    fakeDiv.innerHTML = content;
+    window.requestAnimationFrame(() => {
+      fakeDiv.style.height = fakeDiv.scrollHeight + scrollBarWidth + 'px';
+      cache = Array.from(fakeDiv.querySelectorAll('div')).map(div => {
+        return {
+          height: div.clientHeight,
+          top: div.offsetTop
+        };
+      })
+      fakeDiv.innerHTML = '';
+      render(currentIndex);
+    });
+  }
+  
+  const render = (index) => {
+    let renderContent = '';
+    for (let i = index; i < index + renderCount & i < renderElements.length; i ++ ){
+      // todo: assign attribute
+      renderContent += `<div class="element" style="background: ${renderElements[i].style.background}">${renderElements[i].innerHTML}</div>`
+    }
+    wrapper.innerHTML = renderContent;
+  }
+  
+  container.addEventListener('scroll', () => {
+    window.requestAnimationFrame(() => {
+      const dy = container.scrollTop;
+      const index = cache.findIndex(e => dy + borderTop >= e.top && dy + borderTop <= e.top + e.height) ;
+      if (index === -1) {
+        return;
+      }
+      const currentDiv = cache[index];
+      wrapper.style.transform = `translate(0px, ${-(dy - currentDiv.top + borderTop)}px)`;
+      // only recalculate when index changed
+      if (currentIndex !== index) {
+        render(index);
+      }
+      currentIndex = index;
+    })
+  });
+  
+  preRender();
+}
